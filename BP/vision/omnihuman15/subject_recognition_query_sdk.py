8cf56eac4a48774c28530c46cb41a3ff3b35cd1f
ByteDance <bytedance@ip-192-168-1-143.ap-northeast-1.compute.internal>
2025-10-24 01:04:59 +0900

    add codes using byteplus_sdk

---
+++ BP/vision/omnihuman15/subject_recognition_query_sdk.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+from __future__ import print_function
+
+from byteplus_sdk import visual
+from byteplus_sdk.visual.VisualService import VisualService
+import os
+
+if __name__ == '__main__':
+    visual_service = VisualService()
+
+    # call below method if you don't set ak and sk in $HOME/.volc/config
+    visual_service.set_ak(os.environ['BP_ACCESS_KEY'])
+    visual_service.set_sk(os.environ['BP_SECRET_KEY'])
+    
+    # Request Body (Check interface documentation and copy the required parameters) 
+    form = {
+        "req_key": "realman_avatar_picture_create_role_omni_cv",
+        "task_id": os.environ['OMNI_TASK']
+    }
+    resp = visual_service.cv_get_result(form)
+    print(resp)
---
+++ BP/vision/omnihuman15/submit_task_sdk.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+from __future__ import print_function
+
+from byteplus_sdk import visual
+from byteplus_sdk.visual.VisualService import VisualService
+import os
+
+if __name__ == '__main__':
+    visual_service = VisualService()
+
+    # call below method if you don't set ak and sk in $HOME/.volc/config
+    visual_service.set_ak(os.environ['BP_ACCESS_KEY'])
+    visual_service.set_sk(os.environ['BP_SECRET_KEY'])
+    
+    # Request Body (Check interface documentation and copy the required parameters) 
+    form = {
+        "req_key": "realman_avatar_picture_omni15_cv",
+        "image_url": os.environ['OMNI_IMAGE'],
+        # "mask_url": os.environ['MASK_URLS'],
+        "audio_url": os.environ['OMNI_VOICE'],
+        # "prompt": os.environ['OMNI_PROMPT'],
+        "pe_fast_mode": False
+    }
+    resp = visual_service.cv_submit_task(form)
+    print(resp)
